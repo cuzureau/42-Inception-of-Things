99e0567ca19f6d68e254571c92945047114b5d32
Leonardo <lmunoz-q@student.42.fr>
2023-11-29 17:02:05 +0100

    rm iso

---
+++ start_vm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+qemu-system-x86_64 \
+		-hda ~/goinfre/iot.qcow2 \
+		-enable-kvm \
+		-machine q35 \
+		-cpu host \
+		-m 12G \
+		-smp $(nproc) \
+		-net user,hostfwd=tcp::5000-:22,hostfwd=tcp::8080-:80 \
+		-net nic \
