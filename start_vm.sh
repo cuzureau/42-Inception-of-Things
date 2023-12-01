f40a7fd1bebcb75c93183cc7a96341ec2c07f0c0
Leonardo <lmunoz-q@student.42.fr>
2023-12-01 14:13:29 +0100

    real conf

--- start_vm.sh
+++ start_vm.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-
 qemu-system-x86_64 \
-		-hda ~/goinfre/iot.qcow2 \
-		-enable-kvm \
-		-machine q35 \
-		-cpu host \
-		-m 12G \
-		-smp $(nproc) \
-		-net user,hostfwd=tcp::5000-:22,hostfwd=tcp::8080-:80 \
-		-net nic \
+                -hda ~/goinfre/iot.qcow2 \
+                -enable-kvm \
+                -machine q35 \
+                -cpu host \
+                -m 12G \
+                -smp $(nproc) \
+                -net user,hostfwd=tcp::5000-:22,hostfwd=tcp::8080-:80 \
+                -net nic \
+                -cdrom ~/sgoinfre/debian-12.2.0-amd64-netinst.iso
