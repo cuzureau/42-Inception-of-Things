00e1bff54b9bec77fb5c84c5ade27a7976ad6cad
Christophe Uzureau <cuzureau@student.42.fr>
2023-12-13 11:36:18 +0100

    Update server_install.sh - print log

--- p1/scripts/server_install.sh
+++ p1/scripts/server_install.sh
@@ -1,5 +1,7 @@
 source ../confs/env.sh
 
+echo "[$(hostname)] Installing K3S on controller."
+
 # Telechargement et installation de K3s
 curl -sfL https://get.k3s.io |
 sh -s - --write-kubeconfig-mode 644 \
@@ -15,3 +17,5 @@ done
 
 # Copie du token K3s dans le dossier partage entre toutes les VMs
 sudo cp $PATH_K3S_TOKEN $PATH_SHARED_FOLDER
+
+echo "[$(hostname)] Configured succesfully"
