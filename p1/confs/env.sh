b5ab7dfbf136d04f109d9444be348942d0c9dbe9
Christophe Uzureau <cuzureau@student.42.fr>
2023-12-13 11:46:52 +0100

    Update env.sh - delete spaces

--- p1/confs/env.sh
+++ p1/confs/env.sh
@@ -1,12 +1,9 @@
 export SERVER_NAME="cuzureauS" # "Server"
 export AGENT_NAME="cuzureauSW" # "Server Worker"
-
 export SERVER_IP="192.168.56.110"
 export AGENT_IP="192.168.56.111"
-
 export VM_PROVIDER="virtualbox"
 export VM_MEMORY=1024 # RAM
 export VM_CPUS=1
-
 export PATH_SHARED_FOLDER="/vagrant"
 export PATH_K3S_TOKEN="/var/lib/rancher/k3s/server/token"
