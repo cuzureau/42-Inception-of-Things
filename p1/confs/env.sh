e7163d817ff10c0f1f067b163e2cfdb1b24df50d
Christophe Uzureau <cuzureau@student.42.fr>
2023-12-13 11:04:43 +0100

    Create env.sh

---
+++ p1/confs/env.sh
@@ -0,0 +1,11 @@
+export MASTER_NAME="cuzureauS" # VM "Server"
+export WORKER_NAME="cuzureauSW" # VM "Server Worker"
+
+export MASTER_IP="192.168.56.110"
+export WORKER_IP="192.168.56.111"
+
+export VM_PROVIDER="virtualbox"
+export VM_MEMORY=1024 # RAM
+export VM_CPUS=1
+
+export PATH_SHARED_FOLDER="/vagrant"
