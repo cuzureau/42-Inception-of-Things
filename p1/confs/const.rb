c1a62b21ebc2ca406e9ea5d45b79a3252724f064
Christophe Uzureau <cuzureau@student.42.fr>
2023-12-14 10:09:32 +0100

    Update const.rb

--- p1/confs/const.rb
+++ p1/confs/const.rb
@@ -11,5 +11,8 @@ VM_CPUS = 1
 SYNCED_FOLDER = "shared"
 K3S_TOKEN = "/var/lib/rancher/k3s/server/node-token"
 
+SERVER_SCRIPT_PATH = "./scripts/server_install.sh"
+AGENT_SCRIPT_PATH = "./scripts/agent_install.sh"
+
 SERVER_ENV = {"SERVER_IP"=>SERVER_IP, "K3S_TOKEN"=>K3S_TOKEN, "SYNCED_FOLDER"=>SYNCED_FOLDER}
 AGENT_ENV = {"AGENT_IP"=>AGENT_IP, "SERVER_IP"=>SERVER_IP, "SYNCED_FOLDER"=>SYNCED_FOLDER}
