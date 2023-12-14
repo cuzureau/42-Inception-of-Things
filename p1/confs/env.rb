9cf4c1c03e16a97e889ee92a232850572bd85523
Christophe Uzureau <cuzureau@student.42.fr>
2023-12-14 09:26:07 +0100

    Create env.rb

---
+++ p1/confs/env.rb
@@ -0,0 +1,12 @@
+SERVER_NAME = "cuzureauS" # "Server"
+AGENT_NAME = "cuzureauSW" # "Server Worker"
+
+SERVER_IP = "192.168.56.110"
+AGENT_IP = "192.168.56.111"
+
+VM_PROVIDER = "virtualbox"
+VM_MEMORY = 1024 # RAM
+VM_CPUS = 1
+
+SYNCED_FOLDER = "shared"
+K3S_TOKEN = "/var/lib/rancher/k3s/server/node-token"
