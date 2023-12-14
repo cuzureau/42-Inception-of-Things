c40f49d942782ecbb4136509a3eac928eaf5fbcc
Christophe Uzureau <cuzureau@student.42.fr>
2023-12-14 10:21:19 +0100

    Update agent_install.sh

--- p1/scripts/agent_install.sh
+++ p1/scripts/agent_install.sh
@@ -4,7 +4,6 @@ echo "[$(hostname)] Installing K3S on agent."
 curl -sfL https://get.k3s.io |
 sh -s - agent \
     --node-ip $AGENT_IP \
-    --flannel-iface=eth1 \
     --server "https://$SERVER_IP:6443" \
     --token-file /$SYNCED_FOLDER/node-token
 
