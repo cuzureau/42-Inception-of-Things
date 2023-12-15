d8031524ce9fc00a6fc4de8c6abc0d30ddbc759f
Christophe Uzureau <cuzureau@student.42.fr>
2023-12-15 15:58:51 +0100

    Create const.rb

---
+++ p2/confs/const.rb
@@ -0,0 +1,11 @@
+VM_BOX_NAME = "generic/alpine319" # Alpine Linux LTS
+SERVER_NAME = "cuzureauS" # "Server"
+SERVER_IP = "192.168.56.110"
+
+VM_PROVIDER = "virtualbox"
+VM_MEMORY = 1024 # RAM
+VM_CPUS = 1
+
+SYNCED_FOLDER = "shared"
+
+SERVER_SCRIPT_PATH = "./scripts/server_install.sh"
